dfed8d2bb218c0eacc2aa82886ad7e4ab47452a9
Al Madireddy <al.madireddy@gmail.com>
2018-10-26 14:37:39 -0500

    testing deployment again

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ get_header();
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-      <h1>Hello from Al, just testing deployment a fifth time.</h1>
+      <h1>Hello from Al, just testing deployment a sixth time.</h1>
 		<?php
 		if ( have_posts() ) :
 
