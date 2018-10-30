01c30764f2a50a0552f29b60107df4efc77ae661
Al Madireddy <al.madireddy@gmail.com>
2018-10-30 14:55:06 -0500

    added front-page.php

--- index.php
+++ index.php
@@ -17,7 +17,6 @@ get_header();
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-      <h1>Hello from Al, just testing deployment a seventh time.</h1>
 		<?php
 		if ( have_posts() ) :
 
