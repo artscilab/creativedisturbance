9e264958ff9d4f62dea4a21ea22bec2cd4f41113
Al Madireddy <al.madireddy@gmail.com>
2018-11-15 18:05:21 -0600

    fixed links on series page

--- template-parts/content-podcast.php
+++ template-parts/content-podcast.php
@@ -10,13 +10,14 @@
 <?php
 if ( is_singular() ) :
   set_query_var('display_title', the_title('', '', false));
-  get_template_part('template-parts/title');
-else: ?>
-<div class="podcast-archive-item">
-  <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <a class="title-link" href="<?php get_permalink(the_ID()) ?>"><?php the_title('<h2  class="podcast-archive-title">', '</h2>'); ?>
-    </a>
-    <div class="entry-content">
+  get_template_part('template-parts/title-podcast-single');
+
+  $seriesName = get_the_terms(get_the_ID(), 'series')[0]->name;
+  ?>
+
+<div class="container podcast-single-body">
+  <div class="row justify-content-center">
+    <div class="col-sm-8">
       <?php
       the_content( sprintf(
         wp_kses(
@@ -30,12 +31,21 @@ else: ?>
         ),
         get_the_title()
       ) );
-
-      wp_link_pages( array(
-        'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'creativedisturbance' ),
-        'after'  => '</div>',
-      ) );
       ?>
+    </div>
+  </div>
+</div>
+
+<?php else: ?>
+<div class="podcast-archive-item">
+  <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <a class="title-link" href="<?php the_permalink(get_the_ID()) ?>"><?php the_title('<h2  class="podcast-archive-title">', '</h2>'); ?>
+    </a>
+    <div class="entry-content">
+      <?php
+      the_excerpt();
+      ?>
+      <a class="btn btn-primary" href="<?php the_permalink(get_the_ID()) ?>">Listen -></a>
     </div><!-- .entry-content -->
   </article>
 </div>
---
+++ template-parts/title-podcast-single.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aahladmadireddy
+ * Date: 11/9/18
+ * Time: 3:09 PM
+ */
+
+  $term = get_the_terms(get_the_ID(), 'series')[0];
+  $seriesName = $term->name;
+  $media_id = get_term_meta( $term->term_id, 'podcast_series_image_settings', true );
+  $image_attributes = wp_get_attachment_image_src( $media_id, 'large' );
+  $src = $image_attributes[0];
+?>
+
+<div class="jumbotron jumbotron-fluid jumbotron-title jumbotron-podcast-single">
+  <div class="container">
+    <div class="row series-name">
+      <div class="col-sm-12">
+        <h2><?php echo $seriesName ?></h2>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12">
+        <h1 class="display-3"><?php echo get_the_title() ?></h1>
+      </div>
+    </div>
+  </div>
+</div>
