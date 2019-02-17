6ddd8033c8904294fb1d85daed542e24908bb10d
Al Madireddy <al.madireddy@gmail.com>
2019-02-16 20:41:00 -0600

    added you may also like functionality

--- single-podcast.php
+++ single-podcast.php
@@ -21,58 +21,53 @@ get_header();
     $podcast = pods('podcast', get_the_ID());
     $categories = $podcast->field('podcast_category');
 
+    $similarEpisodes = array();
+
     foreach ($categories as $c) {
       $name = $c['post_title'];
       $podParams = array(
-        "limit" => 1,
+        "limit" => 3,
         "where" => 'podcast_category.post_title = \''. $name .'\''
       );
       $pod = pods('podcast', $podParams);
-      echo $pod->display('post_title');
+
+      while($pod->fetch()) {
+        $series = get_the_terms($pod->display('ID'), 'series')[0];
+        $seriesName = $series->name;
+        $media_id = get_term_meta( $series->term_id, 'podcast_series_image_settings', true );
+        $image_attributes = wp_get_attachment_image_src( $media_id, 'large' );
+        $src = $image_attributes[0];
+        array_push($similarEpisodes, array(
+          "name" => $pod->display('post_title'),
+          "link" => $pod->display('guid'),
+          "categories" => $pod->display('podcast_category'),
+          "src" => $src
+        ));
+      }
     }
 
     $host = $podcast->field('hosts');
-    if (!empty($host)):
+    $voice = $podcast->field('voices');
+
     $hosts = array();
-    foreach ($host as $h) {
-      $hostPod = pods('host', $h['ID']);
-      $name = $hostPod->display('post_title');
-      $link = $hostPod->display('guid');
-      $job = $hostPod->display('job_title');
-      $src = $hostPod->display('profile_photo');
-      array_push($hosts, array(
-        "name" => $name,
-        "link" => $link,
-        "job" => $job,
-        "src" => $src
-      ));
-    }?>
+    $voices = array();
 
-    <div class="container voices podcast-hosts">
-      <div class="row title-row text-center">
-        <div class="col-sm-12">
-          <h2>Hosts</h2>
-        </div>
-      </div>
-      <div class="row justify-content-center voices-row">
-        <?php foreach ($hosts as $h): ?>
-          <div class="voice col-sm-3">
-            <a href="<?php echo $h['link'] ?>">
-              <img src="<?php echo $h['src'] ?>" alt="<?php echo $h['name'] ?>" class="img-fluid profile-img">
-              <h2><?php echo $h['name'] ?></h2>
-              <p class="lead"><?php echo $h['job'] ?></p>
-            </a>
-          </div>
-        <?php endforeach; ?>
-      </div>
-    </div>
-    <?php endif; ?>
-
-
-
-    <?php $voice = $podcast->field('voices');
-    if (!empty($voice)):
-      $voices = array();
+    if (!empty($host)) {
+      foreach ($host as $h) {
+        $hostPod = pods('host', $h['ID']);
+        $name = $hostPod->display('post_title');
+        $link = $hostPod->display('guid');
+        $job = $hostPod->display('job_title');
+        $src = $hostPod->display('profile_photo');
+        array_push($hosts, array(
+          "name" => $name,
+          "link" => $link,
+          "job" => $job,
+          "src" => $src
+        ));
+      }
+    }
+    if (!empty($voice)) {
       foreach ($voice as $v) {
         $voicePod = pods('voice', $v['ID']);
         $name = $voicePod->display('post_title');
@@ -86,25 +81,74 @@ get_header();
           "src" => $src
         ));
       }
-    ?>
-    <div class="container voices podcast-voices">
-      <div class="row title-row text-center">
-        <div class="col-sm-12">
-          <h2>Voices in this episode</h2>
-        </div>
-      </div>
-      <div class="row justify-content-center voices-row">
-        <?php foreach ($voices as $v): ?>
-          <div class="voice col-sm-3">
-            <a href="<?php echo $v['link'] ?>">
-              <img src="<?php echo $v['src'] ?>" alt="<?php echo $v['name'] ?>" class="img-fluid profile-img">
-              <h2><?php echo $v['name'] ?></h2>
-              <p class="lead"><?php echo $v['job'] ?></p>
-            </a>
+    }?>
+
+    <div class="container podcast-hosts-voices">
+      <div class="row">
+        <div class="col">
+          <div class="row title-row text-center">
+            <div class="col-sm-12">
+              <h2>Hosts</h2>
+            </div>
           </div>
-        <?php endforeach; ?>
+          <div class="row justify-content-center voices-row">
+            <?php foreach ($hosts as $h): ?>
+              <?php if (!empty($voices)) { ?>
+                <div class="voice col-sm-6">
+              <?php } else {?>
+                <div class="voice col-sm-3">
+              <?php } ?>
+                <a href="<?php echo $h['link'] ?>">
+                  <img src="<?php echo $h['src'] ?>" alt="<?php echo $h['name'] ?>" class="img-fluid profile-img">
+                  <h2><?php echo $h['name'] ?></h2>
+                  <p class="lead"><?php echo $h['job'] ?></p>
+                </a>
+              </div>
+            <?php endforeach; ?>
+          </div>
+        </div>
+        <?php if (!empty($voices)): ?>
+        <div class="col">
+          <div class="row title-row text-center">
+            <div class="col-sm-12">
+              <h2>Voices in this episode</h2>
+            </div>
+          </div>
+          <div class="row justify-content-center voices-row">
+            <?php foreach ($voices as $v): ?>
+              <div class="voice col-sm-6">
+                <a href="<?php echo $v['link'] ?>">
+                  <img src="<?php echo $v['src'] ?>" alt="<?php echo $v['name'] ?>" class="img-fluid profile-img">
+                  <h2><?php echo $v['name'] ?></h2>
+                  <p class="lead"><?php echo $v['job'] ?></p>
+                </a>
+              </div>
+            <?php endforeach; ?>
+          </div>
+        </div>
+        <?php endif; ?>
       </div>
     </div>
+
+    <?php if (!empty($similarEpisodes)): ?>
+      <div class="container podcast-similar">
+        <div class="row title-row text-center">
+          <div class="col-sm-12">
+            <h2>You May Also Like</h2>
+          </div>
+        </div>
+        <div class="row justify-content-center similar-row">
+          <?php foreach ($similarEpisodes as $similar): ?>
+            <div class="similar col-sm-4">
+              <a href="<?php echo $similar['link'] ?>">
+                <img src="<?php echo $similar['src'] ?>" alt="<?php echo $similar['name'] ?>" class="img-fluid">
+                <h2><?php echo wp_trim_words($similar['name'], 5) ?></h2>
+                <p class="lead"><?php echo $similar['categories'] ?></p>
+              </a>
+            </div>
+          <?php endforeach; ?>
+        </div>
+      </div>
     <?php endif; ?>
 
 		<?php endwhile; // End of the loop.
