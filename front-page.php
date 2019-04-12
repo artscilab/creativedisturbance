30d5e5ab37b3027c23f63c89e59bd4d23092fd4e
Al Madireddy <al.madireddy@gmail.com>
2019-04-12 17:10:11 -0500

    recently updated people on home page

--- front-page.php
+++ front-page.php
@@ -1,6 +1,8 @@
 <?php
 get_header();
 
+
+
 $channels = get_terms(array(
   'taxonomy' => 'series',
   'hide_empty' => false
@@ -32,6 +34,40 @@ while ( $podcasts->fetch() ) {
     array_push($featuredPosts, $post);
   }
 }
+
+$recentHosts = array();
+$recentParams = array(
+  'limit' => '5',
+  'orderby' => 't.post_modified DESC'
+);
+$recents = pods('podcast', $recentParams);
+
+while ($recents->fetch()) {
+  $hosts = $recents->field('hosts');
+  if ($hosts != false) {
+    foreach ($hosts as $host) {
+      array_push($recentHosts, $host);
+    }
+  }
+}
+
+$recentHosts = array_unique($recentHosts, SORT_REGULAR);
+
+$mapFunc = function($recent) {
+  $pod = pods('host', $recent['ID']);
+  $name = $pod->display('post_title');
+  $job= $pod->display('job_title');
+  $src = $pod->display('profile_photo');
+  $guid = $pod->display('guid');
+
+  return array(
+    "name" => $name,
+    "job" => $job,
+    "src" => $src,
+    "link" => $guid
+  );
+};
+$recentHosts = array_map($mapFunc, $recentHosts);
 ?>
   <div class="container">
     <div class="row site-title justify-content-center text-center">
@@ -43,6 +79,26 @@ while ( $podcasts->fetch() ) {
       </div>
     </div>
   </div>
+
+  <div class="container-fluid home-recent-people-container">
+    <div class="row justify-content-center">
+      <h2 class="mb-5">Recently updated</h2>
+    </div>
+    <div class="row hosts-row justify-content-center">
+      <?php foreach ($recentHosts as $host): ?>
+        <div class="host col-sm-2">
+          <a href="<?php echo $host['link'] ?>">
+            <img src="<?php echo $host['src'] ?>" alt="" class="profile-img img-fluid">
+            <div class="profile-info">
+              <h3><?php echo $host['name'] ?></h3>
+              <p class="lead"><?php echo $host['job'] ?></p>
+            </div>
+          </a>
+        </div>
+      <?php endforeach; ?>
+    </div>
+  </div>
+
   <div class="container-fluid home-map-container">
     <div class="row justify-content-center">
       <h2 class="mb-5">Explore the world of Creative Disturbance!</h2>
