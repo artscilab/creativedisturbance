cd250a73db26ff41470cd570479973e8470b4f31
Al Madireddy <al.madireddy@gmail.com>
2019-05-17 17:53:52 -0500

    remove "series: " text in series title

--- functions.php
+++ functions.php
@@ -93,6 +93,25 @@ if ( ! function_exists( 'creativedisturbance_setup' ) ) :
 endif;
 add_action( 'after_setup_theme', 'creativedisturbance_setup' );
 
+function my_theme_archive_title( $title ) {
+  if ( is_category() ) {
+    $title = single_cat_title( '', false );
+  } elseif ( is_tag() ) {
+    $title = single_tag_title( '', false );
+  } elseif ( is_author() ) {
+    $title = '<span class="vcard">' . get_the_author() . '</span>';
+  } elseif ( is_post_type_archive() ) {
+    $title = post_type_archive_title( '', false );
+  } elseif ( is_tax() ) {
+    $title = single_term_title( '', false );
+  }
+
+  return $title;
+}
+
+add_filter( 'get_the_archive_title', 'my_theme_archive_title' );
+
+
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
  *
