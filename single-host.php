7e6d9a310c15d9961adcbc98c1f5bc426af6cb03
Nimitha Jammula <nimitha.jammula@hotmail.com>
2020-09-18 09:13:04 -0500

    Adding functionality of single host

--- single-host.php
+++ single-host.php
@@ -1,27 +1,25 @@
-<?php
-/**
- * The template for displaying all single posts
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
- *
- * @package CreativeDisturbance
- */
-
+<?php /* Template Name: The Disturbed 2*/
 get_header();
-?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-      <?php
+   
+?>
+<div class="the-disturbed">
+  <?php
       while ( have_posts() ) :
         the_post();
         $host = pods('host', get_the_ID());
-
+        
         $jobTitle = $host->display('job_title');
         $src = $host->display('profile_photo');
         $website = $host->display('website');
-        $categories = $host->display('expertise');
+        $exp = $host->display('expertise');
+        $org = $host->display('organization');
         $series = $host->field('series');
+        $country = $host->display('country');
+        $language = $host->display('language');
+
+        $categories = $host -> field('categories');
+        $description = $host->display('description');
         $episodes = $host->field(array(
           "name" => 'podcast',
           "raw" => false,
@@ -33,98 +31,86 @@ get_header();
           $episodes = array_reverse($episodes);
         }
 
-        set_query_var('display_title', the_title('', '', false));
-        set_query_var('job_title', $jobTitle);
-        get_template_part('template-parts/title', "voice"); ?>
+        // set_query_var('display_title', the_title('', '', false));
+        // set_query_var('job_title', $jobTitle);
+        // get_template_part('template-parts/title', "voice"); 
+        ?>
+  <div class="row" style="border-bottom: 4px solid  #FCB054;">
 
-      <div class="container">
-        <div class="row row-mb align-items-center justify-content-center">
-          <div class="col-sm-5 d-flex justify-content-center align-items-center">
-            <img src="<?php echo $src ?>" alt="" class="img-fluid rounded-circle">
-          </div>
-        </div>
-        <div class="row row-mb align-items-center justify-content-center">
-          <div class="col-sm-8 bio-info">
-            <?php if ($website !== ""): ?>
-            <h4>Website</h4>
-            <p><a href="<?php echo $website ?>"><?php echo $website ?></a></p>
-            <?php endif; ?>
-            <?php if ($categories != ""): ?>
-            <h4>Expertise</h4>
-            <p><?php echo $categories ?></p>
-            <?php endif; ?>
-            <h4>Bio</h4>
-            <?php the_content(); ?>
-          </div>
-        </div>
+    <div class="col-4 pro-pic">
+      <center>
+        <img src="<?php echo $src ?>" style="border-radius: 50%; width:200px;
+                                  height:200px;"/>
+              <div class="row" style="padding-top:20px;">
+                <?php if ($website !== ""): ?>
+                <a class="col-3" href="<?php echo $website ?>"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+                <?php endif; ?>
+                <a class="col-3" href="https://www.linkedin.com/in/aadhavansibi"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                <a class="col-3" href="https://www.linkedin.com/in/aadhavansibi"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+                <a class="col-3" href="https://www.linkedin.com/in/aadhavansibi"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+            </div>
+         </center>
+    </div>
+    <div class="col-8 pro-data" >
+      <div class="float-right">
+        Team Member
+       </div>
+       <h4 style="padding-bottom:20px;"><?php echo get_the_title(); ?></h4>
+       <p class="team-member-position">
+               <i class="fa fa-briefcase"aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;<?php echo $jobTitle ?>
+            </p>
+            <p class="team-member-position">
+              <i class="fa fa-map-marker" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<?php  echo $org ?>
+            </p>
+            <p class="team-member-position">
+              <i class="fa fa-book" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;&nbsp;<?php echo $exp ?>
+            </p>
+            <p class="team-member-position">
+              <i class="fa fa-globe" aria-hidden="true"></i>
+                &nbsp;&nbsp;&nbsp;<?php echo $country ?>; <?php echo $language ?>
+            </p>
+        <?php
+        if(!empty($categories)) {
+      foreach ($categories as $c) {
+        $name = $c['post_title'];
+        echo '<div class="topic">';
+        echo $name;
+       echo '</div>';
+     }}
+         ?>
+       
+      <!--  <div class="topic">
+        Science
+       </div>
+       <div class="topic">
+        Culture & Society
+       </div> -->
+    </div>
+  </div>
+  <div class="row pro-description">
+    <?php echo $description ?>
+    <h4 style="text-decoration:underline;text-decoration-color:#FCB054;margin:auto; padding:30px;">Related podcasts</h4>
 
-        <hr>
-
-        <div class="row row-mb">
-          <div class="col-sm-12 text-center">
-            <h2>Host of</h2>
-          </div>
-        </div>
-
-        <?php foreach ($series as $s):
+     <?php foreach ($series as $s):
           $id = intval($s['term_id']);
           $media_id = get_term_meta( $id, 'podcast_series_image_settings', true );
           $image_attributes = wp_get_attachment_image_src( $media_id, 'large' );
           $language = get_term_meta( $id, 'language', true );
           $src = $image_attributes[0]; ?>
+    <div class="row podcasts" style="border-bottom: 4px solid  #FCB054;">
+      <div class="col-4" style="padding:0px;">
+        <img src="<?php echo $src ?>" style="width:100%;height:100%;"/>
+      </div>
+      <div class="col-8 podcast-desc">
+        <h4>Host Of:  <?php echo $s['name'] ?></h4>
+        <?php echo wp_trim_words($s['description'], 40) ?>
+      </div>
+    </div>
 
-          <div class="row row-mb justify-content-center">
-            <div class="col-sm-4">
-              <img src="<?php echo $src ?>" alt="" class="img-fluid">
-            </div>
-            <div class="col-sm-6">
-              <h3>
-                <a href="<?php echo get_term_link($id) ?>"><?php echo $s['name'] ?></a>
-              </h3>
-              <p class="lead"><?php echo $language['post_title'] ?></p>
-              <p><?php echo wp_trim_words($s['description'], 70) ?></p>
-            </div>
-          </div>
-        <?php endforeach; ?>
+    <?php endforeach; ?>
+  </div>
+  <?php endwhile; ?>
 
-        <?php if (!empty($episodes)): ?>
-        <hr>
-
-        <div class="row row-mb">
-          <div class="col-sm-12 text-center">
-            <h2>Recent Episodes</h2>
-          </div>
-        </div>
-
-        <div class="row justify-content-center">
-          <?php for($i = 0; $i < 3; $i++):
-            if (isset($episodes[$i])):
-              $id = intval($episodes[$i]['pod_item_id']);
-              $episodePod = pods('podcast', $id);
-              $link = $episodePod->display('guid');
-              $title = $episodePod->display('post_title');
-              $series = get_the_terms($id, 'series')[0];
-              $seriesName = $series->name;
-              $media_id = get_term_meta( $series->term_id, 'podcast_series_image_settings', true );
-              $image_attributes = wp_get_attachment_image_src( $media_id, 'large' );
-              $src = $image_attributes[0];
-            ?>
-          <div class="col col-sm-4">
-            <a href="<?php echo $link ?>" class="link-block link-channel recent-podcast">
-              <img src="<?php echo $src ?>" alt="" class="img-fluid">
-              <h3><?php echo $title ?></h3>
-              <p class="lead text-muted"><?php echo $seriesName ?></p>
-            </a>
-          </div>
-          <?php endif; endfor; ?>
-        </div>
-        <?php endif; ?>
-      </div> <!-- Container -->
-      <?php endwhile; ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
+</div>
 <?php
-get_sidebar();
 get_footer();
